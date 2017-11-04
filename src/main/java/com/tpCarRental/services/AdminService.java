5c672a8297ac792193c5ec05e60e8236cb150970
Ernst95 <ernst.chapman@gmail.com>
2017-11-04 17:04:57 +0200

    Initial commit

---
+++ src/main/java/com/tpCarRental/services/AdminService.java
@@ -0,0 +1,10 @@
+package com.tpCarRental.services;
+
+import com.tpCarRental.domain.Admin;
+
+/**
+ * Created by Ernst on 2017/08/14.
+ */
+public interface AdminService extends BaseService<Admin, String> {
+
+}
